499edb102597dd166ca176329bc92bcab4a9e6ad
Sam Odum <sam.odum1@gmail.com>
2019-11-22 07:07:34 +0100

    bug(route): remove multer from create-user route

--- routes/apiV1/authRoute.js
+++ routes/apiV1/authRoute.js
@@ -1,5 +1,4 @@
 const { Router } = require('express');
-const Multer = require('../../middleware/Multer');
 const { Auth } = require('../../middleware/Auth');
 const employees = require('../../controllers/employees');
 const roles = require('../../controllers/roles');
@@ -7,8 +6,8 @@ const userRoles = require('../../controllers/gifs');
 
 const router = new Router();
 
-router.post('/supr', Multer.any(), Auth.isSuperAdmin, employees.createBare);
-router.post('/create-user', Multer.any(), Auth.verifyToken, Auth.isAdmin, employees.createBare);
+router.post('/supr', Auth.isSuperAdmin, employees.createBare);
+router.post('/create-user', Auth.verifyToken, Auth.isAdmin, employees.createBare);
 router.delete('/delete-user', Auth.verifyToken, Auth.isAdmin, employees.deleteBare);
 router.post('/sign-in', employees.signin);
 router.post('/create-roles', Auth.verifyToken, Auth.isAdmin, roles.create);
